9f306134fe3e18e536066057d832fda5a9b5f184
priyanshkh <103640136+priyanshkh@users.noreply.github.com>
2022-06-22 21:59:53 +0530

    Create employmanagement.c

---
+++ employmanagement.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<string.h>
+#define ARRAY_SIZE 2
+#define NAME_SIZE 10
+struct Employee
+{
+    int id;
+    char name[NAME_SIZE];
+    float salary;
+};
+int main()
+{
+    struct Employee emp[ARRAY_SIZE];
+    int index = 0;;
+    for(index = 0; index < ARRAY_SIZE; ++index )
+    {
+        printf("\nEnter details of Employee %d\n\n", index+1);
+        printf("Enter id number: ");
+        scanf("%d", &emp[index].id);
+        fflush(stdin);
+        printf("Enter Emp name: ");
+        if(fgets(emp[index].name,NAME_SIZE,stdin) == NULL)
+        {
+            printf("Error in reading the string\n");
+            exit(1);
+        }
+        else
+        {
+            char *p = strchr(emp[index].name, '\n');
+            if (p)
+            {
+                *p = '\0';
+            }
+        }
+        printf("Enter Emp Salary: ");
+        scanf("%f", &emp[index].salary);
+    }
+    printf("\n\n");
+    printf("Emp Name\tId\tSalary\n");
+    for(index = 0; index < ARRAY_SIZE; ++index )
+    {
+        printf("%s\t\t%d\t%.2f\n",
+               emp[index].name, emp[index].id, emp[index].salary);
+    }
+    return 0;
+}
